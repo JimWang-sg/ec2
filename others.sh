7d2712a368f8790ba657e6d23be0dfdafe86cf71
JimWang-sg <130452374+JimWang-sg@users.noreply.github.com>
2024-01-18 16:26:22 +0800

    Update others.sh

--- others.sh
+++ others.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #安装模块
-pip install pandas akshare matplotlib requests mplfinance websockets jupyterlab-language-pack-zh-CN python-okx jupyter_ai
+pip install pandas akshare matplotlib requests mplfinance websockets jupyterlab-language-pack-zh-CN python-okx jupyter_ai openai
